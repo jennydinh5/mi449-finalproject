bf873afa4d946d37bfdf8a308a09871d5a7c23c6
Your Name <jennydinh5@gmail.com>
2023-05-01 09:08:05 -0400

    changes

--- src/script.ts
+++ src/script.ts
@@ -68,3 +68,27 @@ function populatedUI(artist: any) {
     document.getElementById("artistUrl")!.setAttribute("href", artist.href);
 }
 
+let loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
+
+  if (username.value == "" || password.value == "") {
+    alert("Ensure you input a value in both fields!");
+  } else {
+   
+    alert("This form has been successfully submitted!");
+    console.log(
+      `This form has a username of ${username.value} and password of ${password.value}`
+    );
+
+    username.value = "";
+    password.value = "";
+  }
+});
+
+
+
